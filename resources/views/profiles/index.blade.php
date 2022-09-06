3edc875f07e9f75bf7cafdee33c90850bb7ba729
Aswad Ali <aswadali310@gmail.com>
2022-09-07 03:21:22 +0500

    caption page

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -31,7 +31,9 @@
     <div class="row pt-5">
         @foreach($user->posts as $post)
         <div class="col-4 pb-4">
-            <img src="/storage/{{ $post->image }}" class="w-100" alt="">
+            <a href="/p/{{$post->id}}">
+                <img src="/storage/{{ $post->image }}" class="w-100" alt="">
+            </a>
         </div>
         @endforeach
     </div>
