ec32a7f81c6331e828bc74349d94560bef42dd27
Aswad Ali <aswadali310@gmail.com>
2022-09-09 20:20:34 +0500

    Edit Profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,10 +11,16 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="/p/create">Add New Post</a>
+                
+                @can('update', $user->profile)
+                    <a href="/p/create">Add New Post</a>
+                @endcan
+
             </div>
 
+            @can('update', $user->profile)
             <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+            @endcan
 
             <div class="d-flex">
                 <div class="pe-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
